eb200ff3ef104c31606433686cea338f3bbf0d74
Emerson Trindade <emersont@outlook.com.br>
2021-12-10 14:43:10 -0300

    Refactored: Added Key Attribute On Entity.Id
    Added Key Attribute for Id in Entities Table

--- src/Blog.Kernel.Domain/DomainObjects/Entity.cs
+++ src/Blog.Kernel.Domain/DomainObjects/Entity.cs
@@ -1,6 +1,9 @@
-﻿namespace Blog.Kernel.Domain.DomainObjects;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Blog.Kernel.Domain.DomainObjects;
 
 public abstract class Entity
 {
+    [Key]
     public int Id { get; set; }
 }
