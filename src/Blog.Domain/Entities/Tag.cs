ab9d5a51dbc344be31eb1febdba3a627cc88438a
Emerson Trindade <emersont@outlook.com.br>
2021-12-10 15:07:58 -0300

    Created: Entity Tag
    Created a new entity with title Tag for represent a Post Tags in domain context.

---
+++ src/Blog.Domain/Entities/Tag.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using Blog.Kernel.Domain.DomainObjects;
+
+namespace Blog.Domain.Entities;
+
+[Table("[Tag]")]
+public class Tag : Entity
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string Slug { get; set; }
+}
