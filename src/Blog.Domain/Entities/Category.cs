6ee950d9fc277497c3514cd7ac4d106baca53a67
Emerson Trindade <emersont@outlook.com.br>
2021-12-10 14:59:28 -0300

    Created: Entity Category.cs
    Created a new entity with title Category.cs for represent a Post Category in Domain Context.

---
+++ src/Blog.Domain/Entities/Category.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Blog.Domain.Entities;
+
+[Table("[Category]")]
+public class Category
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string Slug { get; set; }
+}
